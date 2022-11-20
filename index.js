6a488f1e28fa837ea6de2000f8827df1efe04ffc
Yarosurawa <yarosurawa@gmail.com>
2022-11-20 12:20:09 +0200

    фщоіва

--- index.js
+++ index.js
@@ -132,3 +132,18 @@ document.querySelectorAll('#box').forEach(box => {
         }
     }
 });
+
+//-----------------------show-on-scroll---------------------------
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenEl = document.querySelectorAll('.hidden')
+hiddenEl.forEach(el => observer.observe(el));
